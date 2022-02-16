6fa1d57dbb959caab7a59e2caa9db25699491972
Rafael Colmenarez <acosta15.88@gmail.com>
2022-02-16 18:26:29 -0400

    Update Index.js
    Error in the caledar

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const date = new Date();
 
+const renderCalendar = () => {
+    
 date.setDate(1);
-
-console.log(date.getDate());
 
 const monthsDays = document.querySelector('.days');
 
-const lastDays = new Date(date.getFullYear(), date.getMonth() + 1,  0).getDate();
+const lastDays = new Date(
+    date.getFullYear(), 
+    date.getMonth() + 1, 0
+    ).getDate();
 
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(),  0).getDate();
+const prevLastDay = new Date(
+    date.getFullYear(), 
+    date.getMonth(), 0
+    ).getDate();
 
 const firstDayIndex = date.getDate();
+
+const lastDayIndex = new Date(
+    date.getFullYear(), 
+    date.getMonth() + 1, 0
+    ).getDate();
+
+const nextDays = 7 - lastDayIndex -1;
 
 const months = [
     "January",
@@ -29,7 +42,7 @@ const months = [
 
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-document.querySelector(".date p").innerHTML = date.toDateString();
+document.querySelector(".date p").innerHTML = new Date().toDateString();
 
 let days = "";
 
@@ -38,6 +51,28 @@ for(let x = firstDayIndex; x > 0; x--){
 }
 
 for (let i = 1;i <= lastDays;i++) {
-    days += `<div> ${i} </div>`;
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class="today"> ${i} </div>`; 
+    } else {
+        days += `<div> ${i} </div>`;
+    }
+}
+
+for (let j = 1;j <= nextDays;j++) {
+    days += `<div class="next-date"> ${j} </div>`;
     monthsDays.innerHTML = days;
 }
+}
+
+
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+})
+
+renderCalendar();
